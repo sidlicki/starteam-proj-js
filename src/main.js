48c2984a7accab1a770d0565fe707089843c5ab2
sidlicki <130383232+sidlicki@users.noreply.github.com>
2023-09-27 15:50:12 +0200

    create and settings
    creating files/folders
    connecting libraries
    adding icons/images
    adding fonts

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './js/abc-search';
+import './js/create-abc';
+import './js/fetch-api';
+import './js/light-dark';
+import './js/mobile-menu';
+import './js/modal-coctail';
+import './js/modal-ingredient';
+import './js/search-by-name';
+import './js/pagination';
+// додаткові підключення робіть під цією стрічкою
