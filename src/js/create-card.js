97ee39cc0eb89afe720b47d203a0f7eed2d5e6ed
sidlicki <130383232+sidlicki@users.noreply.github.com>
2023-09-30 19:58:41 +0200

    s

--- src/js/create-card.js
+++ src/js/create-card.js
@@ -4,7 +4,7 @@ function createMarkup(arr) {
       ({ description, drink, drinkThumb, _id }) =>
         `
     <div class="cocktail-card" id="${_id}">
-            <img class="cocktail-image" src="${drinkThumb}" alt="${drink}" loading="lazy" width="307" height="257"/>
+            <img class="cocktail-image" src="${drinkThumb}" onerror="this.src='https://bibliotekant.pl/wp-content/uploads/2021/04/placeholder-image.png'" alt="${drink}" loading="lazy" width="307" height="257"/>
             <h3 class="cocktail-name">${drink}</h3>
             <p class="cocktail-info">${description}</p>
             <div class="btns-info">
