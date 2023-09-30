4716899204434776ff379ad80e9bb8d5b5f7517b
AndiiSiryi <130078717+AndiiSiryi@users.noreply.github.com>
2023-09-30 23:30:20 +0300

    added function for cocktail list generation and card rendering.

--- src/js/favorite-cocktails.js
+++ src/js/favorite-cocktails.js
@@ -1,87 +1,62 @@
-console.log('привіт');
-// import { renderCocktail } from "./render_function_for_cocktail";
-// import "./cocktails-modal";
-// import { resetPagination, generatePagination } from './pagination';
+import { fetchCocktailDetails } from "./cocktail-api";
+import spriteUrl from '../img/svg/sprite.svg';
 
-// =======================================================================================//
-
-// Функція для збереження даних в localStorage
-function saveToLocalStorage(key, data) {
-    localStorage.setItem(key, JSON.stringify(data));
-}
-
-// Функція для отримання даних з localStorage
-function getFromLocalStorage(key) {
-    const data = localStorage.getItem(key);
-    return data ? JSON.parse(data) : null;
-}
-
-// Очікуємо завантаження DOM перед викликом функції
 document.addEventListener('DOMContentLoaded', generateCocktails);
-
-// Головна функція для генерації списку коктейлів
-function generateCocktails() {
-    // Отримуємо елемент ul з класом "fav-cocktail-list"
-    const favCocktailList = document.querySelector('.fav-cocktail-list');
-
-    // Отримуємо дані з локального сховища
-    const localFavorite = JSON.parse(localStorage.getItem('favoriteList'));
-
-    // Виводимо блок "not-found" або список коктейлів залежно від умов
-    if (localFavorite === null || localFavorite.favoriteCocktails.length <= 0) {
-        // Якщо немає, виводимо блок "not-found" та ховаємо список коктейлів
-        favCocktailList.style.display = 'none';
-        generateNotFoundBlock();
-    } else {
-        // Якщо є, відображаємо список коктейлів та ховаємо блок "not-found"
-        favCocktailList.style.display = 'block';
-        document.querySelector('.not-found').style.display = 'none';
-
-      
-        // Отримуємо список улюблених коктейлів
-        const favoriteCocktails = localFavorite.favoriteCocktails;
-        let drink = {
-            drinks: []
-        };
-
-        // Вибираємо лише ті коктейлі, які мають ідентифікатор
-        favoriteCocktails.forEach(element => {
-            if (element.idDrink !== 0) {
-                drink.drinks.push(element);
-            }
-        });
-
-        // Виводимо коктейлі
-        document.querySelector('.cocktail-list').innerHTML = renderCocktail(sliceArray(drink.drinks)).join('');
+// структура картки 
+const renderCocktail = (arr, container) => {
+        const list = arr.map((item) =>
+            `<li class="cocktail-list-favorite">
+            <img class="cocktail-item-img-favorite" src="${item.drinkThumb}" alt="preview cocktail" width="307" height="auto">
+            <div class="trk">
+            <h2 class="cocktail-name-favorite">${item.drink}</h2>
+            <p class="description-container-favorite">${item.description}</p>
+            <div class="button-wrap-favorite" data-id-drink='${item._id}'>
+                <button type="button" class="learn-more-favorite">LEARN MORE</button>
+                <button type="button" class="remove-favorite">
+                    <svg class="svg-icon-favorite-trash" width="18" height="18">
+                        <use href="${spriteUrl}#icon-trash"></use>
+                    </svg>
+                </button>
+            </div>
+            </div>
+        </li>`).join('');
+        container.insertAdjacentHTML("beforeend", list);
     }
-}
 
-// Функція для генерації блоку "not-found" у випадку відсутності улюблених коктейлів
-function generateNotFoundBlock() {
-    // Вставляємо контент в блок "not-found"
-    document.querySelector('.not-found').innerHTML = `
-        <picture>
-            <!-- DESKTOP -->
-            <source srcset="/img/tablet/coctail.webp 1x, /img/tablet/coctail@2x.webp 2x" type="image/webp" media="(min-width: 1280px)">
-            <!-- TABLET -->
-            <source srcset="/img/tablet/coctail.webp 1x, /img/tablet/coctail@2x.webp 2x" type="image/webp" media="(min-width: 768px)">
-            <!-- MOBILE -->
-            <source srcset="/img/mobile/coctail.webp 1x, /img/mobile/coctail@2x.webp 2x" type="image/webp" media="(min-width: 320px)">
-            <img class="no-fav-cocktail-img" src="/img/tablet/coctail.webp" alt="cocktail">
-        </picture>
-        <p class="no-fav-cocktail-text">
-            You haven't added any
-            <span class="no-fav-cocktail-color">favorite cocktails</span> yet
-        </p>
-    `;
-}
-
-// Функція для генерації обрізаного масиву коктейлів
-function sliceArray(array) {
+// функція для генерації списку коктейлів
+    async function generateCocktails() {
+    
+        const favCocktailList = document.querySelector('.fav-cocktail-list');
+        const notFoundBlock = document.querySelector('.not-found');
+    
+    const localFavorite = JSON.parse(localStorage.getItem('favoriteList')) || [];
+    console.log(localFavorite);
+        if (localFavorite.length > 0) {
+            notFoundBlock.classList.add('is-hidden');
+            console.log(localFavorite.length);
+            const data = await fetchCocktailDetails(localFavorite.map(cocktail => cocktail));
+            console.log(data);
+            renderCocktail(data, favCocktailList);
+            
+        } else {
+            notFoundBlock.classList.remove('is-hidden');
+            notFoundBlock.innerHTML = `
+            <picture>
+                <!-- DESKTOP -->
+                <source srcset="/img/tablet/coctail.webp 1x, /img/tablet/coctail@2x.webp 2x" type="image/webp" media="(min-width: 1280px)">
+                <!-- TABLET -->
+                <source srcset="/img/tablet/coctail.webp 1x, /img/tablet/coctail@2x.webp 2x" type="image/webp" media="(min-width: 768px)">
+                <!-- MOBILE -->
+                <source srcset="/img/mobile/coctail.webp 1x, /img/mobile/coctail@2x.webp 2x" type="image/webp" media="(min-width: 320px)">
+                <img class="no-fav-cocktail-img" src="/img/tablet/coctail.webp" alt="cocktail">
+            </picture>
+            <p class="no-fav-cocktail-text">
+                You haven't added any
+                <span class="no-fav-cocktail-color">favorite cocktails</span> yet
+            </p>
+        `;
+            notFoundBlock.style.display = 'block';
+            favCocktailList.innerHTML = ''; 
+        }
    
-    const start = (currPage - 1) * pageSize;
-    const end = start + pageSize;
-    const res = array.slice(start, end);
-    return { drinks: res };
 }
-
