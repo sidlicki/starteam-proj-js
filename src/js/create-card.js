d2042f78c8babc6a1589e53d876782976308c709
VitalyGotsmanuk <v.gotsmanyuk@gmail.com>
2023-10-01 01:27:29 +0300

    0.3

--- src/js/create-card.js
+++ src/js/create-card.js
@@ -6,7 +6,7 @@ function createMarkup(arr) {
         `
     <li class="cocktail-card" id="${_id}">
             <img class="cocktail-image" src="${drinkThumb}" onerror="this.src='https://bibliotekant.pl/wp-content/uploads/2021/04/placeholder-image.png'" alt="${drink}" loading="lazy" width="307" height="257"/>
-            <h3 class="cocktail-name">${drink}</h3>
+            <h3 class="cocktail-name">${drink.slice(0, 25)}</h3>
             <p class="cocktail-info">${description.slice(0, 115)+"..."}</p>
             <div class="btns-info">
                 <button type="button" data-action="learnmore" class="js-learn-more learn-more-button" id="${_id}">Learn more</button>
