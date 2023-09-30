a864536a3d49c266fdfd76598ecb597fe6e34843
VitalyGotsmanuk <v.gotsmanyuk@gmail.com>
2023-10-01 01:15:33 +0300

    0.4

--- src/js/create-card.js
+++ src/js/create-card.js
@@ -1,19 +1,19 @@
 function createMarkup(arr) {
     return arr.map(({description, drink, drinkThumb, _id})=>
     `
-    <div class="cocktail-card" id="${_id}">
+    <li class="cocktail-card" id="${_id}">
             <img class="cocktail-image" src="${drinkThumb}" alt="${drink}" loading="lazy" width="307" height="257"/>
             <h3 class="cocktail-name">${drink}</h3>
-            <p class="cocktail-info">${description}</p>
+            <p class="cocktail-info">${description.slice(0, 115)+"..."}</p>
             <div class="btns-info">
-                <button type="button" class="js-learn-more-button" id="${_id}">Learn more</button>
-                <button type="button" class="js-add-to-button" id="${_id}">
+                <button type="button" data-action="learnmore" class="js-learn-more learn-more-button" id="${_id}">Learn more</button>
+                <button type="button" data-action="addtofav" class="js-add-to add-to-button" id="${_id}">
                     <svg class="icon-heart" width="20px" height="16px">
-                        <use href="/img/svg/sprite.svg#icon-heart"></use>
+                        <use href="./img/svg/sprite.svg#icon-heart"></use>
                     </svg>
                 </button>
             </div>
-        </div>
+        </li>
     `
     ).join(``);
 };
