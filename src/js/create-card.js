3f2d7a4357b249d4c69b23df3cf115aba4842491
sidlicki <130383232+sidlicki@users.noreply.github.com>
2023-09-30 15:50:46 +0200

    Update create-card.js

--- src/js/create-card.js
+++ src/js/create-card.js
@@ -1,6 +1,8 @@
 function createMarkup(arr) {
-    return arr.map(({description, drink, drinkThumb, _id})=>
-    `
+  return arr
+    .map(
+      ({ description, drink, drinkThumb, _id }) =>
+        `
     <div class="cocktail-card" id="${_id}">
             <img class="cocktail-image" src="${drinkThumb}" alt="${drink}" loading="lazy" width="307" height="257"/>
             <h3 class="cocktail-name">${drink}</h3>
@@ -9,13 +11,14 @@ function createMarkup(arr) {
                 <button type="button" class="js-learn-more learn-more-button" id="${_id}">Learn more</button>
                 <button type="button" class="js-add-to add-to-button" id="${_id}">
                     <svg class="icon-heart" width="20px" height="16px">
-                        <use href="/img/svg/sprite.svg#icon-heart"></use>
+                        <use href="./img/svg/sprite.svg#icon-heart"></use>
                     </svg>
                 </button>
             </div>
         </div>
     `
-    ).join(``);
-};
+    )
+    .join(``);
+}
 
-export {createMarkup};
+export { createMarkup };
