e4b5ee2f2c88ee927853f44ac6507f3998f14cda
sidlicki <130383232+sidlicki@users.noreply.github.com>
2023-10-01 11:07:17 +0000

    Deploying to gh-pages from @ sidlicki/starteam-proj-js@e49b01df63d23714d8f1475c7f619c754677abb6 🚀

---
+++ assets/favorite-coctails-b3135db2.js
@@ -0,0 +1,29 @@
+import"./pagination-fa2bbf6c.js";import{f as c,s}from"./sprite-71a01015.js";document.addEventListener("DOMContentLoaded",n);const l=(a,e)=>{const i=a.map(t=>`<li class="cocktail-list-favorite">
+            <img class="cocktail-item-img-favorite" src="${t.drinkThumb}" alt="preview cocktail" width="307" height="auto">
+            <div class="trk">
+            <h2 class="cocktail-name-favorite">${t.drink}</h2>
+            <p class="description-container-favorite">${t.description}</p>
+            <div class="button-wrap-favorite" data-id-drink='${t._id}'>
+                <button type="button" class="learn-more-favorite">LEARN MORE</button>
+                <button type="button" class="remove-favorite">
+                    <svg class="svg-icon-favorite-trash" width="18" height="18">
+                        <use href="${s}#icon-trash"></use>
+                    </svg>
+                </button>
+            </div>
+            </div>
+        </li>`).join("");e.insertAdjacentHTML("beforeend",i)};async function n(){const a=document.querySelector(".fav-cocktail-list"),e=document.querySelector(".not-found"),i=JSON.parse(localStorage.getItem("favoriteList"))||[];if(console.log(i),i.length>0){e.classList.add("is-hidden"),console.log(i.length);const t=await c(i.map(o=>o));console.log(t),l(t,a)}else e.classList.remove("is-hidden"),e.innerHTML=`
+            <picture>
+                <!-- DESKTOP -->
+                <source srcset="/img/tablet/coctail.webp 1x, /img/tablet/coctail@2x.webp 2x" type="image/webp" media="(min-width: 1280px)">
+                <!-- TABLET -->
+                <source srcset="/img/tablet/coctail.webp 1x, /img/tablet/coctail@2x.webp 2x" type="image/webp" media="(min-width: 768px)">
+                <!-- MOBILE -->
+                <source srcset="/img/mobile/coctail.webp 1x, /img/mobile/coctail@2x.webp 2x" type="image/webp" media="(min-width: 320px)">
+                <img class="no-fav-cocktail-img" src="/img/tablet/coctail.webp" alt="cocktail">
+            </picture>
+            <p class="no-fav-cocktail-text">
+                You haven't added any
+                <span class="no-fav-cocktail-color">favorite cocktails</span> yet
+            </p>
+        `,e.style.display="block",a.innerHTML=""}
---
+++ assets/index-5fc73ba0.js
@@ -0,0 +1,15 @@
+import"./pagination-fa2bbf6c.js";import{s as n,a as s}from"./sprite-71a01015.js";const r="/starteam-proj-js/assets/coctail@2x-256bfe20.webp";function l(t){return t.map(({description:a,drink:o,drinkThumb:c,_id:e})=>`
+    <li class="cocktail-card" id="${e}">
+            <img class="cocktail-image" src="${c}" onerror="this.src='${r}'" alt="${o}" loading="lazy" width="307" height="257"/>
+            <h3 class="cocktail-name">${o.slice(0,25)}</h3>
+            <p class="cocktail-info">${a.slice(0,115)+"..."}</p>
+            <div class="btns-info">
+                <button type="button" data-action="learnmore" class="js-learn-more learn-more-button" id="${e}">Learn more</button>
+                <button type="button" data-action="addtofav" class="js-add-to add-to-button" id="${e}">
+                    <svg class="icon-heart" width="18px" height="18px">
+                        <use href="${n}#icon-heart"></use>
+                    </svg>
+                </button>
+            </div>
+        </li>
+    `).join("")}function d(){return window.innerWidth>1280?"9":"8"}const i=document.querySelector(".cocktails-list");let u=d();s(u).then(t=>{i.innerHTML=l(t)});i.addEventListener("click",function(t){if(t.target.dataset.action=="addtofav"&&console.log("Add to Favorite, ID",t.target.id),t.target.dataset.action=="learnmore"){let a=t.target.id;console.log("Learn More, ID",a)}});
