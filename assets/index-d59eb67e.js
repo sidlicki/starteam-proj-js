afe02ac0dcd60b95a82bfea6f6984156a8c995d8
sidlicki <130383232+sidlicki@users.noreply.github.com>
2023-10-01 13:53:18 +0000

    Deploying to gh-pages from @ sidlicki/starteam-proj-js@00a846b17165ed2d742d276bc2660b1b21d15f4c 🚀

--- assets/index-d59eb67e.js
+++ assets/index-d59eb67e.js
@@ -1,13 +1,13 @@
-import"./pagination-cfd5d029.js";import{s as n,a as s}from"./sprite-71a01015.js";const r="/starteam-proj-js/assets/coctail@2x-256bfe20.webp";function l(t){return t.map(({description:a,drink:o,drinkThumb:c,_id:e})=>`
+import"./pagination-2b13e0be.js";import{d as c,s as r,a as s}from"./coctail@2x-17c11c10.js";function l(t){return t.map(({description:a,drink:o,drinkThumb:n,_id:e})=>`
     <li class="cocktail-card" id="${e}">
-            <img class="cocktail-image" src="${c}" onerror="this.src='${r}'" alt="${o}" loading="lazy" width="307" height="257"/>
+            <img class="cocktail-image" src="${n}" onerror="this.src='${c}'" alt="${o}" loading="lazy" width="307" height="257"/>
             <h3 class="cocktail-name">${o.slice(0,25)}</h3>
             <p class="cocktail-info">${a.slice(0,115)+"..."}</p>
             <div class="btns-info">
                 <button type="button" data-action="learnmore" class="js-learn-more learn-more-button" id="${e}">Learn more</button>
                 <button type="button" data-action="addtofav" class="js-add-to add-to-button" id="${e}">
                     <svg class="icon-heart" width="18px" height="18px">
-                        <use href="${n}#icon-heart"></use>
+                        <use href="${r}#icon-heart"></use>
                     </svg>
                 </button>
             </div>
