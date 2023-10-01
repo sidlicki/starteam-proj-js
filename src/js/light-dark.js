a2e78fa8b77832245f1510a6838d3272561192fe
sidlicki <130383232+sidlicki@users.noreply.github.com>
2023-10-01 02:05:21 +0200

    Update light-dark.js

--- src/js/light-dark.js
+++ src/js/light-dark.js
@@ -1,2 +1,29 @@
-// "axios"/ "modern-normalize" /"notiflix" /"tui-pagination" - вже встановлено
-// console.log('hello');
+const inputSwitch = document.querySelector('.switch');
+
+//прописав логіку для перемикача теми, треба зробити порядок в хтмл коді хедера та в цсс хедера, бо то як зараз працює скритий імпут перемикання - це не вірне рішення
+
+//виклик функції котра визначає чи в локал сторейдж є ключ theme зі значенням дарк,
+//якщо так - накидає на боді класс дарк, якщо ні - видаляє клас дарк.
+switchToDarkTheme();
+
+function switchToDarkTheme() {
+  try {
+    if (localStorage.getItem('theme') === 'dark') {
+      document.querySelector('body').classList.add('dark');
+      inputSwitch.checked = true;
+    } else {
+      document.querySelector('body').classList.remove('dark');
+    }
+  } catch (err) {}
+}
+
+//прослуховувач подій на чекбоксі на його зміну, при зміні відбувається івент, котрий додає/видаляє тему дарк
+inputSwitch.addEventListener('change', event => {
+  event.preventDefault();
+  if (localStorage.getItem('theme') === 'dark') {
+    localStorage.removeItem('theme');
+  } else {
+    localStorage.setItem('theme', 'dark');
+  }
+  switchToDarkTheme();
+});
