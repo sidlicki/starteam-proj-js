07965042d083d564771cfc460a5e6d2030c547c3
sidlicki <130383232+sidlicki@users.noreply.github.com>
2023-10-01 20:07:54 +0200

    test

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import './js/create-abc';
 import './js/cocktail-api';
 import './js/light-dark';
 import './js/mobile-menu';
-import './js/modal-coctail';
+// import './js/modal-coctail';
 import './js/modal-ingredient';
 import './js/search-by-name';
 import './js/pagination';
