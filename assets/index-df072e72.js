b4485e9da5b0de6f9dd1084513ff1f5c6049ef8a
sidlicki <130383232+sidlicki@users.noreply.github.com>
2023-10-01 17:59:55 +0000

    Deploying to gh-pages from @ sidlicki/starteam-proj-js@f1192180e5b0632ce51e85bbb28f986df2f977a6 🚀

--- assets/index-df072e72.js
+++ assets/index-df072e72.js
@@ -1,4 +1,4 @@
-import"./mobile-menu-304cd704.js";import{d as c,s as r,a as s}from"./coctail@2x-17c11c10.js";function l(t){return t.map(({description:a,drink:o,drinkThumb:n,_id:e})=>`
+import{a as r}from"./modal-coctail-9f669a42.js";import{d as c,s}from"./coctail@2x-bfdaa116.js";function l(t){return t.map(({description:a,drink:o,drinkThumb:n,_id:e})=>`
     <li class="cocktail-card" id="${e}">
             <img class="cocktail-image" src="${n}" onerror="this.src='${c}'" alt="${o}" loading="lazy" width="307" height="257"/>
             <h3 class="cocktail-name">${o.slice(0,25)}</h3>
@@ -7,9 +7,9 @@ import"./mobile-menu-304cd704.js";import{d as c,s as r,a as s}from"./coctail@2x-
                 <button type="button" data-action="learnmore" class="js-learn-more learn-more-button" id="${e}">Learn more</button>
                 <button type="button" data-action="addtofav" class="js-add-to add-to-button" id="${e}">
                     <svg class="icon-heart" width="18px" height="18px">
-                        <use href="${r}#icon-heart"></use>
+                        <use href="${s}#icon-heart"></use>
                     </svg>
                 </button>
             </div>
         </li>
-    `).join("")}function d(){return window.innerWidth>1280?"9":"8"}const i=document.querySelector(".cocktails-list");let u=d();s(u).then(t=>{i.innerHTML=l(t)});i.addEventListener("click",function(t){if(t.target.dataset.action=="addtofav"&&console.log("Add to Favorite, ID",t.target.id),t.target.dataset.action=="learnmore"){let a=t.target.id;console.log("Learn More, ID",a)}});
+    `).join("")}function d(){return window.innerWidth>1280?"9":"8"}const i=document.querySelector(".cocktails-list");let u=d();r(u).then(t=>{i.innerHTML=l(t)});i.addEventListener("click",function(t){if(t.target.dataset.action=="addtofav"&&console.log("Add to Favorite, ID",t.target.id),t.target.dataset.action=="learnmore"){let a=t.target.id;console.log("Learn More, ID",a)}});
