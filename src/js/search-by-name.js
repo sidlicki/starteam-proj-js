fc1aa46be7e96a58ee07c4e576553195b5f527a5
Bragi1989 <130233226+Bragi1989@users.noreply.github.com>
2023-10-01 15:00:45 +0200

    1

---
+++ src/js/search-by-name.js
@@ -0,0 +1,41 @@
+import { createMarkup } from './create-card';
+import { searchCocktailsByName } from './cocktail-api';
+
+const searchInput = document.querySelector('.search-input');
+const cardList = document.querySelector('.cocktails-list');
+
+const emptySearch = `<p>Sorry, we didn’t find any cocktail for you</p>`;
+
+searchInput.addEventListener('keyup', onChangeInput);
+
+function onChangeInput(event) {
+  event.preventDefault();
+
+  if (event.key === 'Enter') {
+    const inputValue = searchInput.value.trim();
+    searchInput.value = '';
+
+    if (inputValue) {
+      searchCocktailsByName(inputValue)
+        .then(cocktails => {
+          if (cocktails && cocktails.length > 0) {
+            cardList.innerHTML = createMarkup(cocktails);
+          } else {
+            cardList.innerHTML = emptySearch;
+          }
+        })
+        .catch(error => {
+          console.error('Error fetching cocktails:', error);
+          cardList.innerHTML = errorSearch;
+        });
+    }
+  }
+}
+
+
+
+
+
+
+
+
