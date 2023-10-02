abf01c0d18b4c112bd37294fcb5ddf76443fa0af
sidlicki <130383232+sidlicki@users.noreply.github.com>
2023-10-02 16:14:05 +0200

    fixm import

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import './js/cocktail-api';
 import './js/light-dark';
 import './js/mobile-menu';
 // import './js/modal-coctail';
-import './js/modal-ingredient';
+// import './js/modal-ingredient';
 // import './js/search-by-name';
 import './js/pagination';
 // додаткові підключення робіть під цією стрічкою
