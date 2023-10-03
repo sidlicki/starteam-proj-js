341f7cfedc874cf87bfa27f736f6f47beaf565c9
sidlicki <130383232+sidlicki@users.noreply.github.com>
2023-10-03 00:34:35 +0000

    Deploying to gh-pages from @ sidlicki/starteam-proj-js@d437d876e4353395b649537eeae51e89e56ec1f4 🚀

--- assets/favorite-coctails-7e73e1d5.js
+++ assets/favorite-coctails-7e73e1d5.js
@@ -1,4 +1,4 @@
-import{f as d,n as s,a as n,s as v}from"./coctail-e7ab091e.js";import{n as p,a as u,o as m,b as f}from"./coctail@2x-3a14991e.js";const c=document.querySelector(".fav-cocktail-list"),i=document.querySelector(".not-found");document.addEventListener("DOMContentLoaded",l);function r(){document.querySelectorAll(".remove-favorite").forEach(e=>{e.addEventListener("click",()=>{const a=e.id;f(a),l()})})}const k=(t,e)=>{const a=t.map(o=>`<li class="cocktail-list-favorite">
+import{f as d,n as s,a as n,o as v,b as p,s as u}from"./add-remove-favorite-24312c39.js";import{n as m,a as f}from"./coctail@2x-d8dacd4c.js";const c=document.querySelector(".fav-cocktail-list"),i=document.querySelector(".not-found");document.addEventListener("DOMContentLoaded",l);function r(){document.querySelectorAll(".remove-favorite").forEach(e=>{e.addEventListener("click",()=>{const a=e.id;p(a),l()})})}const k=(t,e)=>{const a=t.map(o=>`<li class="cocktail-list-favorite">
             <img class="cocktail-item-img-favorite" src="${o.drinkThumb}" onerror="this.src='${n}'" alt="preview cocktail" width="307" height="auto">
             <div class="trk">
             <h2 class="cocktail-name-favorite">${o.drink.slice(0,25)}</h2>
@@ -7,7 +7,7 @@ import{f as d,n as s,a as n,s as v}from"./coctail-e7ab091e.js";import{n as p,a a
                 <button type="button" data-action="learnmore" class="learn-more-favorite" id="${o._id}">LEARN MORE</button>
                 <button type="button" class="remove-favorite" id="${o._id}">
                     <svg class="svg-icon-favorite-trash" width="18" height="18">
-                        <use href="${v}#icon-trash"></use>
+                        <use href="${u}#icon-trash"></use>
                     </svg>
                 </button>
             </div>
@@ -15,7 +15,7 @@ import{f as d,n as s,a as n,s as v}from"./coctail-e7ab091e.js";import{n as p,a a
         </li>`).join("");e.innerHTML=a,r()};async function l(){const t=JSON.parse(localStorage.getItem("favoriteCocktails"))||[];if(t.length>0){i.classList.add("is-hidden");const e=await d(t.map(a=>a));k(e,c)}else i.classList.remove("is-hidden"),i.innerHTML=`
             <picture>
                 <!-- TABLET -->
-                <source srcset="${p} 1x, ${u} 2x" type="image/webp" media="(min-width: 768px)">
+                <source srcset="${m} 1x, ${f} 2x" type="image/webp" media="(min-width: 768px)">
                 <!-- MOBILE -->
                 <source srcset="${s} 1x, ${n} 2x" type="image/webp" media="(min-width: 320px)">
                 <img class="no-fav-cocktail-img" src="${s}" alt="cocktail">
@@ -24,4 +24,4 @@ import{f as d,n as s,a as n,s as v}from"./coctail-e7ab091e.js";import{n as p,a a
                 You haven't added any
                 <span class="no-fav-cocktail-color">favorite cocktails</span> yet
             </p>
-        `,i.style.display="block",c.innerHTML="";r()}c.addEventListener("click",function(t){const e=t.target.id;switch(t.target.dataset.action){case"learnmore":m(e);break}});
+        `,i.style.display="block",c.innerHTML="";r()}c.addEventListener("click",function(t){const e=t.target.id;switch(t.target.dataset.action){case"learnmore":v(e);break}});
