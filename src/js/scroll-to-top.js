03831729d546ae1aaa6222c4c2fd6275fe6ced47
sidlicki <130383232+sidlicki@users.noreply.github.com>
2023-10-05 22:05:50 +0200

    add scroll button

--- src/js/scroll-to-top.js
+++ src/js/scroll-to-top.js
@@ -3,12 +3,14 @@ const scrollToTopBtn = document.getElementById('scrollToTopBtn');
 window.addEventListener('scroll', () => {
   // Показати/приховати кнопку в залежності від позиції прокрутки
   if (
-    document.body.scrollTop > 1000 ||
-    document.documentElement.scrollTop > 2000
+    document.body.scrollTop > 800 ||
+    document.documentElement.scrollTop > 800
   ) {
-    scrollToTopBtn.style.display = 'block';
+    scrollToTopBtn.style.opacity = '1';
+    scrollToTopBtn.style.pointerEvents = 'auto';
   } else {
-    scrollToTopBtn.style.display = 'none';
+    scrollToTopBtn.style.opacity = '0';
+    scrollToTopBtn.style.pointerEvents = 'none';
   }
 });
 
